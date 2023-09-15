e8bfc3c7ec3f88be57671ab9d2bde1217b26436e
HP <HP@DESKTOP-01GQJS7>
2023-09-15 19:22:37 +0100

    Add project files.

---
+++ LocationsNigeria.LandingPage/Client/Program.cs
@@ -0,0 +1,20 @@
+using LocationsNigeria.LandingPage.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace LocationsNigeria.LandingPage.Client
+{
+  public class Program
+  {
+    public static async Task Main(string[] args)
+    {
+      var builder = WebAssemblyHostBuilder.CreateDefault(args);
+      builder.RootComponents.Add<App>("#app");
+      builder.RootComponents.Add<HeadOutlet>("head::after");
+
+      builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+      await builder.Build().RunAsync();
+    }
+  }
+}
